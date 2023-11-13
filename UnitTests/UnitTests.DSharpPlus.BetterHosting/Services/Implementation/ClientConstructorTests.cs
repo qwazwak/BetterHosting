8f7048ac8d17890e49648631c1b9b3eb0dc67603
Qwazwak <realqwazwak@gmail.com>
2023-11-13 09:36:53 -0600

    Test cleanup

--- UnitTests/UnitTests.DSharpPlus.BetterHosting/Services/Implementation/ClientConstructorTests.cs
+++ UnitTests/UnitTests.DSharpPlus.BetterHosting/Services/Implementation/ClientConstructorTests.cs
@@ -9,17 +9,25 @@ using Microsoft.Extensions.Options;
 
 namespace UnitTests.DSharpPlus.BetterHosting.Services.Implementation;
 
-[TestFixture]
+[TestFixture(TestOf =typeof(ClientConstructor))]
 public class ClientConstructorTests
 {
+    private MockRepository repository = default!;
+    Mock<IOptions<DiscordConfiguration>> mockOptions = default!;
+    Mock<IMasterClientConfigurator> mockMaster = default!;
+    ClientConstructor sut = default!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        repository = new(MockBehavior.Strict);
+        mockMaster = repository.Create<IMasterClientConfigurator>();
+        mockOptions = repository.Create<IOptions<DiscordConfiguration>>(); sut = new(mockOptions.Object, mockMaster.Object);
+    }
+
     [Test]
     public void ConstructorDoesNothing()
     {
-        MockRepository repository = new(MockBehavior.Strict);
-        Mock<IOptions<DiscordConfiguration>> mockOptions = repository.Create<IOptions<DiscordConfiguration>>();
-        Mock<IMasterClientConfigurator> mockMaster = repository.Create<IMasterClientConfigurator>();
-
-        ClientConstructor sut = new(mockOptions.Object, mockMaster.Object);
         Assert.That(sut, Is.Not.Null);
         repository.Verify();
     }
@@ -27,29 +35,20 @@ public class ClientConstructorTests
     [Test]
     public async Task TestConstruction()
     {
-        MockRepository repository = new(MockBehavior.Strict);
-        Mock<IOptions<DiscordConfiguration>> mockOptions = repository.Create<IOptions<DiscordConfiguration>>();
-        DiscordConfiguration config = new();
-        mockOptions.Setup(o => o.Value).Returns(config).Verifiable(Times.Once);
-        TaskCompletionSource tcs = new();
-        Mock<IMasterClientConfigurator> mockMaster = repository.Create<IMasterClientConfigurator>();
         List<DiscordShardedClient> clientCapture = new();
-        mockMaster.Setup(m => m.Configure(Capture.In(clientCapture))).Returns(tcs.Task).Verifiable(Times.Once);
-        ClientConstructor sut = new(mockOptions.Object, mockMaster.Object);
+        DiscordConfiguration config = new();
 
-        Task<DiscordShardedClient> task = sut.ConstructClient();
-        Assert.That(task.IsCompleted, Is.False);
-        repository.Verify();
-        tcs.SetResult();
-        await Task.Delay(TimeSpan.FromMilliseconds(50));
-        DiscordShardedClient result = await task;
+        mockOptions.Setup(o => o.Value).Returns(config).Verifiable(Times.Once);
+        mockMaster.Setup(m => m.Configure(Capture.In(clientCapture))).Returns(Task.CompletedTask).Verifiable(Times.Once);
+
+        DiscordShardedClient result = await sut.ConstructClient();
 
         Assert.Multiple(() =>
         {
+            Assert.That(clientCapture, IsMore.HasSingle.SameAs(result));
             Assert.That(result.GetConfiguration(), Is.SameAs(config));
-            Assert.That(clientCapture, Has.Count.EqualTo(1));
         });
-        Assert.That(clientCapture[0], Is.SameAs(result));
+        repository.Verify();
     }
 }
 
