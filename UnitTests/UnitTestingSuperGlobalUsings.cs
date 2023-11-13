2bba7cb7583a47254d3dd107d90eb66a4b99b508
Qwazwak <realqwazwak@gmail.com>
2023-11-13 09:44:33 -0600

    updated editorconfig

--- UnitTests/UnitTestingSuperGlobalUsings.cs
+++ UnitTests/UnitTestingSuperGlobalUsings.cs
@@ -1,3 +1,7 @@
-﻿global using Moq;
+﻿#pragma warning disable IDE0079 // Supression is needed
+#pragma warning disable IDE0005 // Using directive is unnecessary.
+global using Moq;
 global using NUnit.Framework;
 global using UnitTestHelpers;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
+#pragma warning restore IDE0079 // Supression is needed
