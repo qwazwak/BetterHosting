287a22012bdf2c79569b4b241e1740e79a354857
Qwazwak <realqwazwak@gmail.com>
2023-11-13 09:26:55 -0600

    Split class

--- Library/DSharpPlus.BetterHosting.SlashCommands/BetterHostingSlashCommandsExtensions.cs
+++ Library/DSharpPlus.BetterHosting.SlashCommands/BetterHostingSlashCommandsExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using DSharpPlus.BetterHosting.Services.Interfaces.ExtensionConfigurators;
-using DSharpPlus.BetterHosting.SlashCommands.Services;
+﻿using DSharpPlus.BetterHosting.Services.Interfaces.ExtensionConfigurators;
 using DSharpPlus.BetterHosting.SlashCommands.Services.Configuration;
 using DSharpPlus.BetterHosting.SlashCommands.Services.Implementation;
 using DSharpPlus.SlashCommands;
@@ -21,49 +17,5 @@ public static class BetterHostingSlashCommandsExtensions
     /// </summary>
     /// <param name="services"></param>
     /// <returns>The same <see cref="IServiceCollection"/> for chaining</returns>
-    public static IServiceCollection AddSlashCommands(this IServiceCollection services)
-    {
-        services.AddTransient<IDiscordClientConfigurator, SlashCommandsSetup>();
-        services.AddTransient<IConfigureOptions<SlashCommandsConfiguration>, SlashCommandsOptionsConfiguration>();
-        return services;
-    }
-
-    /// <summary>
-    /// Registers a configurator to register the <typeparamref name="TCommand"/> with <see cref="SlashCommands"/>
-    /// </summary>
-    /// <typeparam name="TCommand"></typeparam>
-    /// <param name="services"></param>
-    public static void RegisterSlashCommand<TCommand>(this IServiceCollection services) where TCommand : ApplicationCommandModule => services.AddTransient<ISlashCommandsExtensionConfigurator, SlashCommandRegisterConfigurator<TCommand>>();
-
-    /// <summary>
-    /// Registers a configurator to register all derivations of <see cref="ApplicationCommandModule"/> with <see cref="SlashCommands"/> from the calling assembly
-    /// </summary>
-    /// <param name="services"></param>
-    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void RegisterSlashCommands(this IServiceCollection services)
-    {
-        ArgumentNullException.ThrowIfNull(services);
-        Assembly callingAssembly = Assembly.GetCallingAssembly();
-        services.RegisterSlashCommandsCore(callingAssembly);
-    }
-
-    /// <summary>
-    /// Registers a configurator to register all derivations of <see cref="ApplicationCommandModule"/> with <see cref="SlashCommands"/> from the given assembly
-    /// </summary>
-    /// <param name="services"></param>
-    public static void RegisterSlashCommands(this IServiceCollection services, Assembly assembly)
-    {
-        ArgumentNullException.ThrowIfNull(services);
-        ArgumentNullException.ThrowIfNull(assembly);
-        services.RegisterSlashCommandsCore(assembly);
-    }
-
-    private static void RegisterSlashCommandsCore(this IServiceCollection services, Assembly assembly) => services.AddSingleton<ISlashCommandsExtensionConfigurator>(new SlashCommandsAutoRegisterConfigurator(assembly));
-#if false
-    public static void AddSlashCommand(this IServiceCollection services, Type commandType)
-    {
-        Type constructed = typeof(SlashCommandRegisterConfigurator<>).MakeGenericType(commandType);
-        services.AddTransient(typeof(ISlashCommandsExtensionConfigurator), constructed);
-    }
-#endif
+    public static IServiceCollection AddSlashCommands(this IServiceCollection services) => services.AddTransient<IDiscordClientConfigurator, SlashCommandsSetup>().AddTransient<IConfigureOptions<SlashCommandsConfiguration>, SlashCommandsOptionsConfiguration>();
 }
