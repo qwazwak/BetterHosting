9ebc4c3c6fdb430be5f1199f834f9e168a772209
Qwazwak <realqwazwak@gmail.com>
2023-11-13 09:45:47 -0600

    reworked TypeHandlerAdder to avoid array allocation

--- Library/DSharpPlus.BetterHosting.CommandsNext/Services/Configuration/TypeHandlerAdder.cs
+++ Library/DSharpPlus.BetterHosting.CommandsNext/Services/Configuration/TypeHandlerAdder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 
@@ -7,17 +8,23 @@ namespace DSharpPlus.BetterHosting.CommandsNext.Services.Configuration;
 
 internal class TypeHandlerAdder : ICommandsNextConfigurator
 {
-    private readonly IEnumerable<Type> handlerTypes;
+    private readonly object handlerTypes;
 
-    public TypeHandlerAdder(IEnumerable<Type> handlerTypes) => this.handlerTypes = handlerTypes;
-    public TypeHandlerAdder(params Type[] handlerTypes) => this.handlerTypes = handlerTypes;
-    public static Func<TypeHandlerAdder> Factory(IEnumerable<Type> handlerType) => () => new(handlerType);
-    public static Func<TypeHandlerAdder> Factory(params Type[] handlerType) => () => new(handlerType);
+    public TypeHandlerAdder(Type handlerType) => handlerTypes = handlerType;
+    public TypeHandlerAdder(ICollection<Type> handlerTypes) => this.handlerTypes = handlerTypes;
 
     public ValueTask Configure(int shardID, CommandsNextExtension extension)
     {
-        foreach (Type handlerType in handlerTypes)
-            extension.RegisterCommands(handlerType);
+        if (handlerTypes is Type singleType)
+        {
+            extension.RegisterCommands(singleType);
+        }
+        else
+        {
+            Debug.Assert(handlerTypes is ICollection<Type>);
+            foreach (Type handlerType in (ICollection<Type>)handlerTypes)
+                extension.RegisterCommands(handlerType);
+        }
         return ValueTask.CompletedTask;
     }
 }
