d01bd6792f73e8523fd5fbccc5bd9c40ab4b2c7e
Qwazwak <realqwazwak@gmail.com>
2023-11-14 16:09:00 -0600

    Removal of unused

--- Library/DSharpPlus.BetterHosting.Lavalink/Services/ILavalinkConfigurationFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using DSharpPlus.Lavalink;
-using Microsoft.Extensions.Configuration;
-
-namespace DSharpPlus.BetterHosting.Lavalink.Options;
-
-internal interface ILavalinkConfigurationFactory
-{
-    void BindToSection(LavalinkConfiguration lavalink, IConfiguration configuration);
-}
