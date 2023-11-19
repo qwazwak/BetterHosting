e0144c0f400f3b90c70eae94cba279d710f24867
Qwazwak <realqwazwak@gmail.com>
2023-11-19 12:23:33 -0600

    Refactorcleanup of DiscordClientHost and made use of BackgroundLifecycleService

--- Library/DSharpPlus.BetterHosting/Services/Hosted/DiscordClientHost.cs
+++ Library/DSharpPlus.BetterHosting/Services/Hosted/DiscordClientHost.cs
@@ -3,12 +3,11 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus.BetterHosting.Services.Interfaces.Internal;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DSharpPlus.BetterHosting.Services.Hosted;
 
-internal class DiscordClientHost : IHostedService
+internal class DiscordClientHost : BackgroundLifecycleService
 {
     private readonly ILogger<DiscordClientHost> logger;
     private readonly IClientManager clientManager;
@@ -23,38 +22,43 @@ internal class DiscordClientHost : IHostedService
         this.clientManager = clientManager;
     }
 
-    public async Task StartAsync(CancellationToken stoppingToken)
+    private async Task<DiscordShardedClient> ConstructClient()
     {
-        Debug.Assert(this.client == null);
-        DiscordShardedClient client;
+        try
+        {
+            return await clientConstructor.ConstructClient();
+        }
+        catch (Exception ex) when (ex is not TaskCanceledException)
+        {
+            logger.LogError(ex, $"There was a problem building/configuring the {nameof(DiscordShardedClient)}. Unable to startup client due to unexpected exception {{type}}: {{message}}", ex.GetType(), ex.Message);
+            throw;
+        }
+    }
+
+    private async Task ConnectClient(DiscordShardedClient client)
+    {
+        try
+        {
+            await client.StartAsync();
+        }
+        catch (Exception ex) when (ex is not TaskCanceledException)
+        {
+            logger.LogError(ex, "Unable to connect because of {message}", ex.Message);
+            throw;
+        }
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.CompletedTask;
+    protected override async Task Start(CancellationToken stoppingToken)
+    {
+        if (client != null)
+            return;
 
         try
         {
-            try
-            {
-                client = await clientConstructor.ConstructClient().WaitAsync(stoppingToken);
-            }
-            catch (Exception ex) when (ex is not TaskCanceledException)
-            {
-                logger.LogError(ex, $"There was a problem building/configuring the {nameof(DiscordShardedClient)}. Unable to startup client due to unexpected exception {{type}}: {{message}}", ex.GetType(), ex.Message);
-                clientManager.SetException(ex);
-                throw;
-            }
-
-            stoppingToken.ThrowIfCancellationRequested();
-
+            client = await ConstructClient().WaitAsync(stoppingToken);
             logger.LogTrace("Connecting to discord...");
-            try
-            {
-                await client.StartAsync().WaitAsync(stoppingToken);
-            }
-            catch (Exception ex) when(ex is not TaskCanceledException)
-            {
-                logger.LogError(ex, "Unable to connect because of {message}", ex.Message);
-                clientManager.SetException(ex);
-                throw;
-            }
-
+            await ConnectClient(client).WaitAsync(stoppingToken);
             logger.LogTrace("Discord connected");
         }
         catch (TaskCanceledException ex)
@@ -62,15 +66,21 @@ internal class DiscordClientHost : IHostedService
             clientManager.SetCancelled(ex.CancellationToken);
             return;
         }
-        clientManager.Set(client);
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unable to startup client because of {message}", ex.Message);
+            clientManager.SetException(ex);
+            throw;
+        }
+
+        Debug.Assert(client != null);
+        if (stoppingToken.IsCancellationRequested)
+            clientManager.SetCancelled(stoppingToken);
+        else
+            clientManager.Set(client);
     }
 
-    /// <summary>
-    /// Triggered when the application host is performing a graceful shutdown.
-    /// </summary>
-    /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
-    /// <returns>A <see cref="Task"/> that represents the asynchronous Stop operation.</returns>
-    public Task StopAsync(CancellationToken cancellationToken)
+    protected override Task Stop(CancellationToken cancellationToken)
     {
         DiscordShardedClient? client = this.client;
         // Stop called without start
