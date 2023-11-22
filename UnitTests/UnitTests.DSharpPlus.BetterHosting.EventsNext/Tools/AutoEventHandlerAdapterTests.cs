7efa5519484206c8389e51c4e70984376dc420c1
Qwazwak <realqwazwak@gmail.com>
2023-11-21 21:17:04 -0600

    Added testing for AutoEventHandlerAdapter.Invoke

---
+++ UnitTests/UnitTests.DSharpPlus.BetterHosting.EventsNext/Tools/AutoEventHandlerAdapterTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.BetterHosting.EventsNext.Services;
+using DSharpPlus.BetterHosting.EventsNext.Tools;
+using DSharpPlus.EventArgs;
+using Moq.AutoMock;
+
+namespace UnitTests.DSharpPlus.BetterHosting.EventsNext.Tools;
+
+[TestOf(typeof(AutoEventHandlerAdapter<,>))]
+[TestFixtureSource(typeof(HandlerTypesTestData), nameof(HandlerTypesTestData.SpecificHandlerInterfacesWithArgumentType))]
+public class AutoEventHandlerAdapterTests<TInterface, TArgument>
+    where TInterface : class, IDiscordEventHandler<TArgument>
+    where TArgument : DiscordEventArgs
+{
+    private static string GetMethodName()
+    {
+        MethodInfo[] methods = typeof(TInterface).GetMethods();
+        Assume.That(methods.Length, Is.EqualTo(1), "Unexpected number of methods found");
+        return methods[0].Name;
+    }
+
+    [Test]
+    public void CallSupportedHandler()
+    {
+        DiscordClient sender = new(new() { Token = "Fake Token" });
+        TArgument argument = ArgumentConstructor<TArgument>.ConstructArgument();
+        string methodName = GetMethodName();
+        Mock<TInterface> mock = new();
+        mock.SetupWithAny<TInterface, ValueTask>(methodName)
+            .Returns(ValueTask.CompletedTask)
+            .Verifiable(Times.Once);
+        ValueTask result = AutoEventHandlerAdapter<TInterface, TArgument>.Invoke(mock.Object, sender, argument);
+        Assert.That(result.IsCompletedSuccessfully, Is.True);
+
+        mock.Verify();
+        IReadOnlyList<object> calledArguments = mock.Invocations[0].Arguments;
+        Assert.Multiple(() =>
+        {
+            Assert.That(calledArguments[0], Is.SameAs(sender));
+            Assert.That(calledArguments[1], Is.SameAs(argument));
+        });
+    }
+}
