1c6cad2c53e7a8b6b175578fde2358806bdb6b84
Qwazwak <realqwazwak@gmail.com>
2023-11-24 22:27:17 -0600

    Replaced source generation with reflection

--- Library/DSharpPlus.BetterHosting.EventsNext/RegistrationExtensions.cs
+++ Library/DSharpPlus.BetterHosting.EventsNext/RegistrationExtensions.cs
@@ -21,19 +21,14 @@ public static partial class RegistrationExtensions
     public static RegistrationBuilder<TInterface> AddEventHandlers<TInterface>(this IServiceCollection services) where TInterface : class, IDiscordEventHandler => new(services);
 
     /// <summary>
-    /// Registers the handler <typeparamref name="TImplementation"/> in the <see cref="IServiceCollection"/> to recieve events for all supported interfaces
+    /// Registers the handler <typeparamref name="TImplementation"/> in the <see cref="IServiceCollection"/> to receive events for all supported interfaces
     /// </summary>
     /// <typeparam name="TImplementation"></typeparam>
     /// <returns>The same <see cref="IServiceCollection"/> for chaining</returns>
-#if SourceGenerated_AutoRegisterHandler
-    public static partial IServiceCollection AutoRegisterHandler<TImplementation>(this IServiceCollection services) where TImplementation : class, IDiscordEventHandler;
-
-#else
     public static IServiceCollection AutoRegisterHandler<TImplementation>(this IServiceCollection services) where TImplementation : class, IDiscordEventHandler
     {
         foreach (Type implementedExactInterface in typeof(TImplementation).GetInterfaces().Where(EventReflection.Validation.IsExactInterface))
             RegistrationBuilderHelper.RegisterHandler(services, implementedExactInterface, HandlerDescriptor.Describe(implementedExactInterface, typeof(TImplementation)), false);
         return services;
     }
-#endif
 }
