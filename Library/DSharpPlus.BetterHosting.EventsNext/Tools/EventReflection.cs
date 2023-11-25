8091a9dbd5d6c065266b77e0370622807c6ee3e8
Qwazwak <realqwazwak@gmail.com>
2023-11-24 22:29:09 -0600

    Moved Verification/Validation/AutoWeakReference to main file

--- Library/DSharpPlus.BetterHosting.EventsNext/Tools/EventReflection.cs
+++ Library/DSharpPlus.BetterHosting.EventsNext/Tools/EventReflection.cs
@@ -5,6 +5,10 @@ using System.Collections.Immutable;
 using System.Collections.Generic;
 using System.Diagnostics;
 using DSharpPlus.BetterHosting.EventsNext.Services.Implementations;
+using DSharpPlus.BetterHosting.EventsNext.Exceptions;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace DSharpPlus.BetterHosting.EventsNext.Tools;
 
@@ -61,5 +65,72 @@ internal static partial class EventReflection
     public static class ManagerHostType
     {
         public static Type For(Type eventInterface) => typeof(EventsNextBackgroundHost<>).MakeGenericType(eventInterface);
+    }
+
+    public static class Verification
+    {
+        public static void VerifyExactInterface<TInterface>() where TInterface : IDiscordEventHandler => VerifyExactInterface(typeof(TInterface));
+        public static void VerifyExactInterface(Type interfaceType)
+        {
+            ArgumentNullException.ThrowIfNull(interfaceType);
+            if (!Validation.IsExactInterface(interfaceType))
+                ThrowInvalidExactInterface(interfaceType);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [DoesNotReturn]
+        private static void ThrowInvalidExactInterface(Type interfaceType) => throw new InvalidHandlerInterfaceException(interfaceType);
+    }
+    public static class Validation
+    {
+        public static bool IsExactInterface<TInterface>() where TInterface : IDiscordEventHandler => IsExactInterface(typeof(TInterface));
+        public static bool IsExactInterface(Type interfaceType)
+        {
+            if (!interfaceType.IsInterface || !typeof(IDiscordEventHandler).IsAssignableFrom(interfaceType))
+                return false;
+            weakDetails.GetTarget(out ImmutableDictionary<Type, DetailsRecord> dict);
+            return dict.ContainsKey(interfaceType);
+        }
+
+        public static bool IsAssignableToAny<THandler>() where THandler : IDiscordEventHandler => IsAssignableToAny(typeof(THandler));
+        public static bool IsAssignableToAny(Type handlerType)
+        {
+            weakDetails.GetTarget(out ImmutableDictionary<Type, DetailsRecord> dict);
+            return dict.Keys.Any(t => t.IsAssignableFrom(handlerType));
+        }
+    }
+
+    private readonly struct AutoWeakReference<T>(Func<T> factory) where T : class?
+    {
+        public AutoWeakReference() : this(Activator.CreateInstance<T>) { }
+
+        private readonly Func<T> factory = factory;
+        private readonly WeakReference<T> weakReference = new(null!);
+
+        //
+        // We are exposing TryGetTarget instead of a simple getter to avoid a common problem where people write incorrect code like:
+        //
+        //      WeakReference ref = ...;
+        //      if (ref.Target != null)
+        //          DoSomething(ref.Target)
+        //
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public readonly void GetTarget(out T target)
+        {
+            if (weakReference.TryGetTarget(out T? cached) && cached != null)
+            {
+                target = cached;
+            }
+            else
+            {
+                T newValue = factory.Invoke();
+                weakReference.SetTarget(newValue);
+                target = newValue;
+            }
+        }
+
+        public readonly void SetTarget(T target) => weakReference.SetTarget(target);
+
+        public readonly void Clear() => weakReference.SetTarget(null!);
     }
 }
