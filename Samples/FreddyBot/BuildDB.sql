580926119313a5a4b7db4979270d753eea65ad89
Qwazwak <realqwazwak@gmail.com>
2023-11-25 08:42:17 -0600

    Simplified packages for eventsnext, updated SQL setup, and started github workflows

--- Samples/FreddyBot/BuildDB.sql
+++ Samples/FreddyBot/BuildDB.sql
@@ -1,11 +1,16 @@
-﻿--CREATE TABLE [Guilds] (
---  [GuildID] BigInt NOT NULL UNIQUE
+﻿BEGIN TRANSACTION;
+
+--CREATE TABLE [Guilds] (
+--  [GuildID] bigint NOT NULL
+--, CONSTRAINT [sqlite_master_PK_Guilds] PRIMARY KEY ([GuildID])
 --);
 
 CREATE TABLE [SwearJars] (
-  [GuildID] BigInt NOT NULL
-, [ValueOfSingleSwear] REAL NOT NULL
-, [SwearCount] INTEGER NOT NULL
-, CONSTRAINT [PK_SwearJars] PRIMARY KEY ([GuildID])
---, FOREIGN KEY(GuildID) REFERENCES Guilds(GuildID)
+  [GuildID] bigint NOT NULL
+, [ValueOfSingleSwear] real NOT NULL
+, [SwearCount] bigint NOT NULL
+, CONSTRAINT [sqlite_autoindex_SwearJars_1] PRIMARY KEY ([GuildID])
+--, FOREIGN KEY ([GuildID]) REFERENCES [Guilds] ([GuildID])
 );
+
+COMMIT;
